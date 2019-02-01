6bc9fc0882da907125778ec0d5c13a1ea4d126af
leo-zz <pylibing@qq.com>
2019-02-01 12:32:24 +0800

    Update AutoConfiguration

--- client/src/main/java/com/xiaoqiang/client/configuration/XiaoQiangAutoConfiguration.java
+++ client/src/main/java/com/xiaoqiang/client/configuration/XiaoQiangAutoConfiguration.java
@@ -4,8 +4,6 @@ import com.xiaoqiang.client.controller.ExceptionController;
 import com.xiaoqiang.client.controller.MonitorController;
 import com.xiaoqiang.client.postprocessor.XiaoQiangRetryBeanPostProcessor;
 import com.xiaoqiang.client.util.XiaoQiangHttpClient;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
@@ -14,21 +12,17 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
+/**
+ * XiaoQiang的自动配置类。
+ *
+ * @Author:leo-zz
+ */
 @Configuration
 @ConditionalOnBean(XiaoQiangMarkerConfiguration.Marker.class)
 public class XiaoQiangAutoConfiguration extends WebMvcConfigurerAdapter {
 
-
-    //注意@Value中# 与 $的区别。
-    // #表示SPEL表达式，主要就是创建一个对象，然后通过对象获取值。
-    // $是占位符，表示属性值。
-//    @Value("${xiaoqiang.xiaoQiangURL}")
-//    private String  xiaoQiangURL;
-
-
-
     /**
-     * 注入bean类的方式
+     * 注入XiaoQiang的JVM信息监控controller，默认自动开启
      *
      * @return
      */
@@ -38,18 +32,30 @@ public class XiaoQiangAutoConfiguration extends WebMvcConfigurerAdapter {
         return new MonitorController();
     }
 
+    /**
+     * 注入XiaoQiang的方法重试监控controller，默认自动开启
+     *
+     * @return
+     */
     @Bean
     @ConditionalOnProperty(prefix = "xiaoqiang.retry", name = "enabled", matchIfMissing = true)
     public ExceptionController ExceptionController() {
         return new ExceptionController();
     }
 
+    /**
+     * 注入向Server端注册、发送心跳、取消注册的bean类
+     *
+     * @return
+     */
     @Bean
-    public XiaoQiangHttpClient XiaoQiangHttpClient(){
+    public XiaoQiangHttpClient XiaoQiangHttpClient() {
         return new XiaoQiangHttpClient();
     }
 
-    //允许跨域访问
+    /**
+     * SpringWeb提供的允许跨域访问的配置
+     */
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
@@ -60,32 +66,44 @@ public class XiaoQiangAutoConfiguration extends WebMvcConfigurerAdapter {
     }
 
 
-    @Configuration
-    protected static class XiaoQiangConfigBeanConfiguration {
-
-        @Bean
-        @ConditionalOnProperty(prefix = "xiaoqiang.retry", name = "enabled", matchIfMissing = true)
-        public XiaoQiangRetryBeanPostProcessor xiaoQiangBeanPostProcessor() {
-            return new XiaoQiangRetryBeanPostProcessor();
-        }
-
-        @Bean
-        @ConditionalOnMissingBean
-        public XiaoQiangMonitorConfigBean xiaoQiangMonitorConfigBean() {
-            return new XiaoQiangMonitorConfigBean();
-        }
-
-        @Bean
-        @ConditionalOnMissingBean
-        public XiaoQiangRetryConfigBean xiaoQiangRetryConfigBean() {
-            return new XiaoQiangRetryConfigBean();
-        }
-
-        @Bean
-        @ConditionalOnMissingBean
-        public XiaoQiangConfigBean xiaoQiangConfigBean() {
-            return new XiaoQiangConfigBean();
-        }
+    /**
+     * 若开启方法错误重试功能，会将XiaoQiang的后置处理器注入的容器中。
+     * 在refresh()过程中检查所有注有@XiaoQiangRetry的类或方法，使用动态代理实现错误重试的逻辑。
+     */
+    @Bean
+    @ConditionalOnProperty(prefix = "xiaoqiang.retry", name = "enabled", matchIfMissing = true)
+    public XiaoQiangRetryBeanPostProcessor xiaoQiangBeanPostProcessor() {
+        return new XiaoQiangRetryBeanPostProcessor();
     }
 
+    /**
+     * 注入XiaoQiangMonitorConfigBean，通过配置选择JVM监控的内容。
+     * @return
+     */
+    @Bean
+    @ConditionalOnMissingBean
+    public XiaoQiangMonitorConfigBean xiaoQiangMonitorConfigBean() {
+        return new XiaoQiangMonitorConfigBean();
+    }
+
+    /**
+     *
+     * 注入XiaoQiangRetryConfigBean，通过配置设定方法错误重试的参数。
+     * @return
+     */
+    @Bean
+    @ConditionalOnMissingBean
+    public XiaoQiangRetryConfigBean xiaoQiangRetryConfigBean() {
+        return new XiaoQiangRetryConfigBean();
+    }
+
+    /**
+     *  注入XiaoQiangConfigBean，通过配置设置Server通信相关的参数。
+     * @return
+     */
+    @Bean
+    @ConditionalOnMissingBean
+    public XiaoQiangConfigBean xiaoQiangConfigBean() {
+        return new XiaoQiangConfigBean();
+    }
 }
